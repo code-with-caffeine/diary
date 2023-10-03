426e07b66fa655332bbe6b612e2ec5add1a959e2
Kshitij <kshitijsinha@gmail.com>
2023-10-03 16:01:53 +0530

    nthng

--- diary/main.js
+++ diary/main.js
@@ -17,8 +17,17 @@ const About = qs('#About')
 htext.addEventListener('click', ()=>{
 	settings.classList.toggle('hide')
 })
-
+function dates(){
 for (var i = 1; i < 31; i++) {
 	dateArea.innerHTML+='<li class="date"><p class="dtext"> Date:&emsp;January&ensp;'+i+',&ensp;2023 </p></li>'
 }
+}
 
+function add_click(){
+const date = qsa('.dtext')
+date.addEventListener('click', () => {
+	date.classList.add('active')
+})
+}
+
+dates().then(add_click())
